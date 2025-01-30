6be5ccab664d0cc66af2367794ca678877154cef
boss <smithsaruni16@gmail.com>
2025-01-30 22:40:51 +0300

    port

--- app.py
+++ app.py
@@ -96,4 +96,7 @@ mail = Mail(app)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+    port = int(os.environ.get("PORT", 5000))  # Render assigns a port dynamically
+    app.run(host="0.0.0.0", port=port)
